62366e45cf07a6a334308344cd5a265917cf2f5f
TeemoTrader <austinnorman1995@gmail.com>
2021-07-17 17:28:52 -0400

    Initial Commit

---
+++ AfricaAnimals/AfricaAnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAnimalsApp.swift
+//  AfricaAnimals
+//
+//  Created by Teemo Norman on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
